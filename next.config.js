8ed22dcfdb278965b26eeec61c87b8b8c5ce5d8e
unknown <henriquem.vdossantos@gmail.com>
2026-01-20 21:44:24 -0300

    update: Firebase

--- next.config.js
+++ next.config.js
@@ -2,18 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    // Se você usava domains, substitua por remotePatterns
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**", // ou especifique os domínios confiáveis
-      },
-    ],
-  },
-  experimental: {
-    // Remova appDir se estiver definido
-    // appDir: true
-  },
+        hostname: "**"
+      }
+    ]
+  }
 };
-
 module.exports = nextConfig;
