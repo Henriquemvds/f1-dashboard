a85b2a7d4b1b06efbe36fc559e47e2c0ab498aec
unknown <henriquem.vdossantos@gmail.com>
2026-04-05 19:49:07 -0300

    update: links social media posts

--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -2,8 +2,8 @@ self.__BUILD_MANIFEST = {
   "/": [
     "static/chunks/pages/index.js"
   ],
-  "/artigo/[id]": [
-    "static/chunks/pages/artigo/[id].js"
+  "/_error": [
+    "static/chunks/pages/_error.js"
   ],
   "__rewrites": {
     "afterFiles": [],
